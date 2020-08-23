795f008675627b03ca4112ffe04993ddbf2e616c
Rami Hikmat <rami@ramihikmat.net>
2020-08-24 05:34:17 +1000

    Basic controller

---
+++ AtheerBackend/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using AtheerCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AtheerBackend.Controllers
+{
+    [Route("api/blog")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            BlogPost post = new BlogPost { Title = "Hi" };
+            return Ok(post);
+        }
+    }
+}
