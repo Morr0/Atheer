e34d2b07bce6b8a7630ad8f55a28929ec2e160fc
Rami Hikmat <rami@ramihikmat.net>
2020-09-01 02:49:02 +1000

    Update non-null only attributes

--- AtheerBlogWriterBackend/Services/BlogPostModelDTOMappingsProfile.cs
+++ AtheerBlogWriterBackend/Services/BlogPostModelDTOMappingsProfile.cs
@@ -10,7 +10,12 @@ namespace AtheerBlogWriteBackend.Models
             CreateMap<BlogPostWriteDTO, BlogPost>();
             CreateMap<BlogPost, BlogPostReadDTO>();
 
-            CreateMap<BlogPostUpdateDTO, BlogPost>();
+            CreateMap<BlogPostUpdateDTO, BlogPost>()
+                .ForAllMembers((opts) =>
+                {
+                    // Make sure to update non-null properties only
+                    opts.Condition((src, dst, srcMemb) => srcMemb != null);
+                });
         }
     }
 }
