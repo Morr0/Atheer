6dcab439b8155b3a4337d8c0b62278af23a6a6f0
Rami Hikmat <rami@ramihikmat.net>
2020-10-02 11:31:08 +1000

    Broken redirection

---
+++ atheer-frontend/src/views/ArticleView.vue
@@ -0,0 +1,24 @@
+<template>
+    <Article :article="article" :showContent="true" />
+</template>
+
+<script>
+import Article from "@/components/Article.vue";
+
+export default {
+    components: {
+        Article
+    },
+    data: function (){
+        console.log(this.$route);
+
+        return {
+            article: []
+        };
+    }
+}
+</script>
+
+<style>
+
+</style>
