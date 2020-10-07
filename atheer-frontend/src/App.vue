d61e3ef9f920cfe14ff91f2e8ba256b4d257b36c
Rami Hikmat <rami@ramihikmat.net>
2020-10-07 11:08:40 +1100

    Simple app bar

--- atheer-frontend/src/App.vue
+++ atheer-frontend/src/App.vue
@@ -1,45 +1,60 @@
 <template>
   <v-app>
-    <!-- <v-app-bar
+    <v-app-bar
       app
-      color="primary"
-      dark
+      light
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <div class="d-flex align-center">
+        <!-- <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+        /> -->
+    </div>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+
+    <!-- To make the title have a link if was not in main view -->
+        <v-card-title v-if="$route.path.length === 1" primary-title>
+            Atheer Blog
+        </v-card-title>
+        <v-card-title v-else primary-title>
+            <router-link :to="{name: `Articles`}">
+                Atheer Blog
+            </router-link>
+        </v-card-title>
+
+
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
         text
+        :to="toApps"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">
+            Apps
+        </span>
       </v-btn>
-    </v-app-bar> -->
+
+      <v-btn
+        text
+        :to="toContact"
+      >
+        <span class="mr-2">
+            Contact me
+        </span>
+      </v-btn>
+    </v-app-bar>
 
     <v-main>
         <!-- <Articles /> -->
-        <router-view></router-view>
+        <v-container grid-list-xs>
+            <router-view></router-view>
+        </v-container>
+        
     </v-main>
   </v-app>
 </template>
@@ -48,14 +63,30 @@
 import Articles from "@/views/Articles.vue";
 
 export default {
-  name: 'App',
+    name: 'App',
 
-  components: {
-      Articles
-  },
-
-  data: () => ({
-    //
-  }),
+    components: {
+        Articles
+    },
+    computed: {
+        toApps: function (){
+            return {
+                name: "ArticleView",
+                params: {
+                    year: "0", 
+                    titleShrinked: "apps"
+                }
+            };
+        },
+        toContact: function (){
+            return {
+                name: "ArticleView",
+                params: {
+                    year: "0", 
+                    titleShrinked: "contact"
+                }
+            };
+        }
+    }
 };
 </script>
