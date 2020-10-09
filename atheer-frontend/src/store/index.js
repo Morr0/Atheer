4f5c37a3422c1cfd3267266387c6ff16f1f9dc98
Rami Hikmat <rami@ramihikmat.net>
2020-10-09 14:09:27 +1100

    Use environment variable

--- atheer-frontend/src/store/index.js
+++ atheer-frontend/src/store/index.js
@@ -3,9 +3,14 @@ import Vuex from "vuex";
 
 Vue.use(Vuex)
 
+const {
+    VUE_APP_API = "http://localhost:5000/"
+} = process.env;
+console.log(process.env.VUE_APP_API);
+
 // Load post utility
 const postsUtil = require("../utils/posts.js");
-postsUtil.init("https://localhost:5001/");
+postsUtil.init(VUE_APP_API);
 
 export default new Vuex.Store({
     state: {
