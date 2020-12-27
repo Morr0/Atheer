ad416eb328d1b3fa7ca3b6fd262646354b2df912
Rami Hadad <rami@ramihikmat.net>
2020-12-27 21:37:05 +1100

    Post request

--- Atheer/Controllers/ArticleController.cs
+++ Atheer/Controllers/ArticleController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using AtheerBackend.DTOs.BlogPost;
+using AtheerBackend.Models;
 using AtheerBackend.Services.BlogService;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -18,14 +19,34 @@ namespace Atheer.Controllers
             _service = service;
         }
 
-        [FromRoute] 
+        [FromRoute]
         public BlogPostPrimaryKey Route { get; set; }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var post = await _service.GetSpecific(Route).ConfigureAwait(false);
             if (post is null) return Redirect("/");
 
+            return View("Article", post);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(string submit, [FromForm] BlogPostPrimaryKey key)
+        {
+            _logger.LogInformation($"Like: {key.CreatedYear}, {key.TitleShrinked}");
+            BlogPost post = null;
+            switch (submit)
+            {
+                case "Like":
+                    post = await _service.Like(key).ConfigureAwait(false);
+                    break;
+                case "Share":
+                    post = await _service.Share(key).ConfigureAwait(false);
+                    break;
+            }
+            if (post is null) return Redirect("Error");
+            
             return View("Article", post);
         }
     }
