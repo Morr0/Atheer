ef331a4166ff4d92874e703953b256be836594d3
Rami Hadad <rami@ramihikmat.net>
2021-01-03 16:17:52 +1100

    Liking and sharing to backend done

---
+++ Atheer/Controllers/ArticleApiController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Atheer.Services;
+using Atheer.Services.BlogService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atheer.Controllers
+{
+    [Route("api/article")]
+    [ApiController]
+    public class ArticleApiController : ControllerBase
+    {
+        private readonly IBlogPostService _service;
+
+        public ArticleApiController(IBlogPostService service)
+        {
+            _service = service;
+        }
+        
+        [HttpPost("like")]
+        public async Task<IActionResult> Like([FromQuery] BlogPostPrimaryKey key)
+        {
+            var post = await _service.GetSpecific(key).ConfigureAwait(false);
+            if (post is null) return BadRequest();
+
+            await _service.Like(key).ConfigureAwait(false);
+            return Ok();
+        }
+        
+        [HttpPost("share")]
+        public async Task<IActionResult> Share([FromQuery] BlogPostPrimaryKey key)
+        {
+            var post = await _service.GetSpecific(key).ConfigureAwait(false);
+            if (post is null) return BadRequest();
+
+            await _service.Share(key).ConfigureAwait(false);
+            return Ok();
+        }
+    }
+}
--- Atheer/wwwroot/js/site.js
+++ Atheer/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const like = (createdYear, titleShrinked) => {
+    const origin = window.location.origin;
+    const url = `${origin}/api/article/like?createdYear=${createdYear}&titleShrinked=${titleShrinked}`;
+    
+    $.ajax({
+        type: "POST",
+        url: url,
+        success: (result) => {
+            let likes = Number.parseInt($("#likes")[0].innerText);
+            likes++;
+            $("#likes")[0].innerText = `${likes.toString()} Likes | `;
+        }
+    });
+};
+
+const share = (createdYear, titleShrinked) => {
+    const origin = window.location.origin;
+    const url = `${origin}/api/article/share?createdYear=${createdYear}&titleShrinked=${titleShrinked}`;
+    $.ajax({
+        type: "POST",
+        url
+    });
+};
