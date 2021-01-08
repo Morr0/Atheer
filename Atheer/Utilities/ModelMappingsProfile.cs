5624e07ab106aa98accf5f58710921e93ff7b861
Rami Hikmat <rami@ramihikmat.net>
2021-01-09 07:51:54 +1100

    Further refactorings

---
+++ Atheer/Utilities/ModelMappingsProfile.cs
@@ -0,0 +1,23 @@
+﻿using Atheer.Controllers.ViewModels;
+using Atheer.Models;
+using AutoMapper;
+
+namespace Atheer.Utilities
+{
+    public class ModelMappingsProfile : Profile
+    {
+        public ModelMappingsProfile()
+        {
+            TakeCareOfViewModels();
+        }
+        
+        void TakeCareOfViewModels()
+        {
+            // Post
+            CreateMap<BlogPostEditViewModel, BlogPost>().ReverseMap();
+            
+            // User
+            CreateMap<RegisterViewModel, User>();
+        }
+    }
+}
