737727833f24509695b6af2a937149071b6361cd
Rami Hikmat <rami@ramihikmat.net>
2021-01-09 06:54:06 +1100

    Edit link view component

---
+++ Atheer/ViewComponents/ArticleEditLinkModel.cs
@@ -0,0 +1,17 @@
+﻿using Atheer.Models;
+
+namespace Atheer.ViewComponents
+{
+    public class ArticleEditLinkModel
+    {
+        public ArticleEditLinkModel(BlogPost post)
+        {
+            CreatedYear = post.CreatedYear;
+            TitleShrinked = post.TitleShrinked;
+        }
+        
+        public int CreatedYear { get; set; }
+        public string TitleShrinked { get; set; }
+        
+    }
+}
---
+++ Atheer/ViewComponents/ArticleEditLinkViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Atheer.ViewComponents
+{
+    public class ArticleEditLinkViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(ArticleEditLinkModel model)
+        {
+            return View("Default", model);
+        }
+    }
+}
