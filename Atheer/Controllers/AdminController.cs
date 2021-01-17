677f1573195611abcad0a7cb2a38eebea4a6dd7c
Rami Hadad <rami@ramihikmat.net>
2021-01-17 23:39:41 +1100

    Empty admin controller

---
+++ Atheer/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Atheer.Services.UserService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atheer.Controllers
+{
+    [Route("Admin")]
+    [Authorize(Roles = UserRoles.AdminRole)]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("/");
+        }
+    }
+}
