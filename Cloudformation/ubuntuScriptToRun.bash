465606f99ce9fd4ec488a883d49221ed3f713876
Rami Hikmat <rami@ramihikmat.net>
2021-02-14 21:18:04 +1100

    Kind of working user data on EC2

--- Cloudformation/ubuntuScriptToRun.bash
+++ Cloudformation/ubuntuScriptToRun.bash
@@ -3,6 +3,7 @@ apt-get update -y
 apt-get upgrade -y
 apt install git -y
 apt install tmux -y
+apt install postgresql-client -y
 # Install dotnet - BEGIN
 # https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#2004-
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
@@ -21,5 +22,11 @@ mkdir atheer
 cd atheer
 git clone https://github.com/Morr0/Atheer .
 cd Atheer
+dotnet restore
+# Install dotnet-ef to make the database up-to-date
+dotnet tool uninstall --global dotnet-ef
+dotnet tool install --global dotnet-ef
+export ASPNETCORE_ENVIRONMENT="Production"
+dotnet ef database update
 # Create a detached tmux session and run the website in it
 tmux new-session -d -s server "dotnet run --environment Production"
